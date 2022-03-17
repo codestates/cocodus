b7e8bf190a98ab936fb4486b0d84a19f79505b2d
happy5happy5 <zombil8731@gmail.com>
2022-03-17 20:26:02 +0900

    Add: API 라우팅 기능 추가

---
+++ server/controllers/board/cmt.js
@@ -0,0 +1,8 @@
+module.exports = {
+  post: async (req, res) => {
+    res.status(200).send("board yes cmt post");
+  },
+  delete: async (req, res) => {
+    res.status(200).send("board yes cmt delete");
+  },
+};
