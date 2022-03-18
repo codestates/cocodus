162c0cf913d0a04a160742fd1c7a29c9191cce50
happy5happy5 <zombil8731@gmail.com>
2022-03-18 15:47:06 +0900

    jongbok

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/cocodus/server
-node -v
-npm -v
-nvm -v
 npm install
 npm install pm2@latest -g
 sudo apt-get update
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+node -v
+npm -v
+nvm -v
 cd /home/ubuntu/cocodus/server
 pm2 stop server.js 2> /dev/null || true
 pm2 delete server.js 2> /dev/null || true
