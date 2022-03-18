40c91b10c58ab27d942b21dacdadfe16c5edd5b2
happy5happy5 <zombil8731@gmail.com>
2022-03-18 12:38:48 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /home/ubuntu/cocodus/server
-npm start
+/usr/local/bin/pm2 start ~/server.js -i 0 --name "admin" &
