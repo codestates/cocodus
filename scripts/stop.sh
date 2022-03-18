6a32c0c5053ed2e0f6a8d16571f10faa1af429b6
happy5happy5 <zombil8731@gmail.com>
2022-03-18 23:55:35 +0900

    Clean: set -e삭제

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e
-echo "----------------stop.sh start here-------------------"
-node -v
-npm -v
-nvm -v
 cd /home/ubuntu/cocodus/server
 pm2 stop server.js 2> /dev/null || true
 pm2 delete server.js 2> /dev/null || true
