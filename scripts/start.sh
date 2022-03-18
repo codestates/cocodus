3156037d1524ce5f71c56fe6458b76d4a0eb73ce
happy5happy5 <zombil8731@gmail.com>
2022-03-18 12:52:07 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/cocodus/server
-sudo ln -s /home/ubuntu/.nvm/versions/node/v16.14.0/bin/node /usr/bin/node
-/home/ubuntu/.nvm/versions/node/v16.14.0/bin/pm2 start cocodus/server/server.js
+which node
+which pm2
