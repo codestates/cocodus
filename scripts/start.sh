b8b56b62db261a4bed6f2839df1d785f89f81675
happy5happy5 <zombil8731@gmail.com>
2022-03-19 21:46:05 +0900

    Update: test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -e
 cd /home/ubuntu/cocodus/server
 authbind --deep pm2 start server.js
 
-//npx sequelize-cli db:migrate
 pm2 save
 sleep 3s && pm2 status // pm2동작을 확인 - 제대로 작동했는지, codedeploy ->로그 확인
