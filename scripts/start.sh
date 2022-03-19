ef1a968797feb41f59d7e6c21d3117adf0ab5334
happy5happy5 <zombil8731@gmail.com>
2022-03-19 21:40:32 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 cd /home/ubuntu/cocodus/server
-sudo pm2 start server.js
+authbind --deep pm2 start server.js
+
+npx sequelize-cli db:migrate
+pm2 save
+sleep 3s && pm2 status // pm2동작을 확인 - 제대로 작동했는지, codedeploy ->로그 확인
