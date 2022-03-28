ecdcf421e6ccfea439a0667865e2cce8415ad9b0
toystosound <toystosound@gmail.com>
2022-03-28 15:18:19 +0900

    Add: github 로그인 구현

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -11,8 +11,44 @@ module.exports = {
     //   res.status(200).send("github go");
     // } else {
     //   res.status(200).send("test signuppost");
-    // }
-    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8080/user/signup/github`;
+    // }`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8080/user/signup/github`
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`;
     res.redirect(githubAuthUrl);
+  },
+  github: async (req, res) => {
+    const axios = require("axios");
+
+    const { session, query } = req;
+    const { code } = query;
+    //console.log(session, code);
+
+    const test = await axios.post(
+      `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`
+    );
+
+    //console.log(test);
+    //console.log(req);
+    // if (!code) {
+    //   res.status(401).end();
+    // } else {
+    //   res.status(200).send("성공");
+    // }
+    // 메소드는 POST
+    // https://github.com/login/oauth/access_token
+    // 필수 파라메더
+    //client_id = .env에 있음 Required
+    //client_secret = .env에 있음 Required
+    //code 발급받은 code Required
+    //redirectUrl Callback URL 있어야 함
+    //state
+    const tokenString = test.data;
+    let accessToken = tokenString.split("&")[0].split("=")[1];
+    console.log(accessToken);
+    res
+      .status(200)
+      .cookie("accessToken", accessToken)
+      // .send("토큰이가는지보고싶습니다")
+      //.redirect("http://cocodus.site/");
+      .redirect("http://localhost:3000");
   },
 };
