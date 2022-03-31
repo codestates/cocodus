ed021787b5cb18c65964fb92547f50eb0c0f2edd
happy5happy5 <zombil8731@gmail.com>
2022-03-31 14:20:25 +0900

    Add: cocodusId state

--- server/controllers/user/info.js
+++ server/controllers/user/info.js
@@ -1,8 +1,23 @@
+const { User } = require("../../models");
 module.exports = {
   get: async (req, res) => {
     res.status(200).send("test userinfoget");
   },
   patch: async (req, res) => {
+    const name = req.data;
+    const id = req.data;
+    const accesstoken = req.data;
+    let validation = await User.findOne({ where: { id, accesstoken } });
+    if (validation) {
+      User.create(
+        {
+          name: name,
+          //("상호명"+","+"도로명"+","+"")
+        },
+        { fields: ["name"] }
+      );
+    }
+
     res.status(200).send("test userinfopatch");
   },
   delete: async (req, res) => {
