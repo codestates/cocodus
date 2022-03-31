7af51dcbc6fef5ab42f5f7ee3fde4f1640999fb7
toystosound <toystosound@gmail.com>
2022-03-31 14:26:25 +0900

    Add: KaKao

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -67,10 +67,19 @@ export default function NavBar({ isLogin, setIsLogin, setAccessToken }) {
                 style={{ width: "100px", marginLeft: "20px" }}
                 src="Kakaotalk.png"
               />
-              <img
-                style={{ width: "100px", marginLeft: "20px" }}
-                src="Google.png"
-              />
+              <a
+                href={
+                  "https://accounts.google.com/o/oauth2/v2/auth" +
+                  "?client_id=286406699597-7mlmmmhid7n5dph3g3ce3s90do65bk4i.apps.googleusercontent.com" +
+                  "&response_type=code&redirect_uri=http://localhost:8080/user/signup/google" +
+                  "&scope=https://www.googleapis.com/auth/userinfo.email"
+                }
+              >
+                <img
+                  style={{ width: "100px", marginLeft: "20px" }}
+                  src="Google.png"
+                />
+              </a>
             </Modal>
           </Nav>
         </Container>
