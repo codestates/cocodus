5b2a4e326f408b498c33f4ebe7e51c35a8afbd16
happy5happy5 <zombil8731@gmail.com>
2022-03-31 15:20:10 +0900

    Update info.js

--- server/controllers/user/info.js
+++ server/controllers/user/info.js
@@ -4,8 +4,8 @@ module.exports = {
     res.status(200).send("test userinfoget");
   },
   patch: async (req, res) => {
-    if (!req.data) return res.send({}); //바디에 정보가 없을때임
-    if (!(req.data.name && req.data.id && req.data.accesstoken))
+    if (!req.body) return res.send({}); //바디에 정보가 없을때임
+    if (!(req.body.name && req.body.id && req.body.accesstoken))
       return res.send({}); //바디에 name,id,accesstoken이 없으면 안됨
     const { id, accesstoken, name, roadAddress, placeName, x, y } = req.data;
     //여기에서 세션에 저장한 엑세스토큰 유효성 검사해야함
