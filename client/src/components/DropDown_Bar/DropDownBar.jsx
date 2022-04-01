85991a9b03fd43a7802660582a52db59872bd9aa
sun-one95 <isay0693@gmail.com>
2022-04-01 17:45:12 +0900

    Update: DropdownMenu css 수정

--- client/src/components/DropDown_Bar/DropDownBar.jsx
+++ client/src/components/DropDown_Bar/DropDownBar.jsx
@@ -1,31 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import { accessTokenStore } from "../../Store/accesstoken-zustand";
 import { useNavigate } from "react-router-dom";
-
-const Div = styled.div`
-  position: absolute;
-  top: 100%;
-  margin-top: 1rem;
-  right: 0px;
-`;
-
-const Ul = styled.ul`
-  position: relative;
-  z-index: 5;
-  width: 12rem;
-  background: white;
-  box-shadow: 0px 5px 25px rgba(0, 0, 0, 0.15);
-  list-style: none;
-`;
-
-const Li = styled.li`
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  font-weight: 500;
-  cursor: pointer;
-  border-bottom: 1px solid rgba(196, 196, 196, 0.26);
-`;
+import { Div, Ul, Li } from "./DropDownBar.styled";
 
 const DropDownBar = () => {
   let navigate = useNavigate();
