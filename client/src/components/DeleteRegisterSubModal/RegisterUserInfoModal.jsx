d9646da9f2b44d54f7a6d92e6d1cf585bbd20996
sun-one95 <isay0693@gmail.com>
2022-04-01 09:11:05 +0900

    Update: 코드 수정

--- client/src/components/DeleteRegisterSubModal/RegisterUserInfoModal.jsx
+++ client/src/components/DeleteRegisterSubModal/RegisterUserInfoModal.jsx
@@ -27,7 +27,7 @@ function RegisterUserInfoModal({ closeModal }) {
   // 회원 정보 등록하는 함수
   const onRegister = async () => {
     const userData = await axios({
-      method: "PATCH",
+      method: "POST",
       url: "http://localhost:8080/user/info",
       data: {
         accessToken,
