fa37e5776204b328297db36905dde78c103976ad
happy5happy5 <zombil8731@gmail.com>
2022-04-01 09:57:07 +0900

    Update server

--- server/controllers/user/info.js
+++ server/controllers/user/info.js
@@ -4,20 +4,19 @@ module.exports = {
     res.status(200).send("test userinfoget");
   },
   post: async (req, res) => {
-    // console.log(JSON.parse(Object.keys(req.body)[0]));
     let data;
     if (typeof req.body === "object") {
       data = JSON.parse(Object.keys(req.body)[0]);
     }
-    // console.log(data);
-    if (!data) return res.send({}); //바디에 정보가 없을때임
+    if (!data) {
+      console.log("post::user/info 에서 뭔가 잘못됬다 찾아라 이놈아");
+      return res.send({}); //바디에 정보가 없을때임
+    }
     if (!(data.name && data.id && data.accessToken)) return res.send({}); //바디에 name,id,accesstoken이 없으면 안됨
     const { id, accessToken, name, roadAddress, placeName, x, y } = data;
     //여기에서 세션에 저장한 엑세스토큰 유효성 검사해야함
     let validation = await User.findOne({ where: { id } });
-    // console.log({ validation });
     if (validation) {
-      // console.log("here");
       User.update(
         {
           name,
@@ -33,7 +32,7 @@ module.exports = {
         }
       );
 
-      return res.status(200).send("okay");
+      return res.status(201).send("okay");
     } else return res.status(404).send("user dosen't exist");
   },
   delete: async (req, res) => {
