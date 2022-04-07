636ec7ee2d7ce8d75a8dfc7bbbfdc686fcc07072
happy5happy5 <zombil8731@gmail.com>
2022-04-08 08:19:23 +0900

    Add: logout 절름발이 수정

--- client/src/components/DropDown_Bar/DropDownBar.jsx
+++ client/src/components/DropDown_Bar/DropDownBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { accessTokenStore } from "../../Store/accesstoken-zustand";
 import { useNavigate } from "react-router-dom";
 import { Div, Ul, Li } from "./DropDownBar.styled";
+import { registerUserInfoStore } from "../../Store/RegisterUserInfo-zustand";
 
 const DropDownBar = () => {
   let navigate = useNavigate();
@@ -12,11 +13,15 @@ const DropDownBar = () => {
   //     dispatch(clearStep());
   //     authService.resetToken();
   //   };
-  const { isLogin, chgIsLogin, chgAccToken } = accessTokenStore();
+  const { nickName, chgInput } = registerUserInfoStore();
+  const { isLogin, chgIsLogin, chgAccToken, chgCocoId } = accessTokenStore();
   const logoutHandle = () => {
     window.document.cookie = "accessToken" + "=; Max-Age=-99999999;";
+    window.document.cookie = "cocodusId" + "=; Max-Age=-99999999;";
     chgIsLogin(false);
     chgAccToken("");
+    chgInput("");
+    chgCocoId("");
   };
 
   return (
