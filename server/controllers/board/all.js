906231bc0b6991b1004efad22862a77dceac11a0
happy5happy5 <zombil8731@gmail.com>
2022-04-07 17:52:29 +0900

    Update all.js

--- server/controllers/board/all.js
+++ server/controllers/board/all.js
@@ -2,7 +2,7 @@ const { sortDist } = require("../database");
 const { User, Post, Post_tag } = require("../../models");
 module.exports = {
   get: async (req, res) => {
-    let { isLogin, accessToken, cocodusId, nickName } = req.query;
+    let { isLogin, accessToken, cocodusId, nickName, howMany } = req.query;
     if (isLogin === "true" && accessToken && cocodusId && nickName) {
       let userLoc = await User.findOne({
         where: { id: cocodusId },
@@ -26,14 +26,27 @@ module.exports = {
         result.push(temp);
       }
       let cal = await Promise.all(result);
-      return res.status(200).send(cal.map((x) => x.dataValues));
+      return res
+        .status(200)
+        .send(
+          cal
+            .filter((x, i) =>
+              i >= howMany[0] && i < howMany[0] + howMany[1] ? true : false
+            )
+            .map((x, i) => x.dataValues)
+        );
     }
     if (isLogin === "false" && !accessToken && !nickName) {
-      let temp = await Post.findAll({ attributes: ["jsonfile"] });
+      let temp = await Post.findAll({
+        attributes: ["jsonfile"],
+        offset: Number(howMany[0]) || 0,
+        limit: Number(howMany[1]),
+        subQuery: false,
+      });
 
-      return res.send(temp);
+      return res.status(200).send(temp);
     }
     console.log("이 말이 나온다면 클라에서 useEffect관리해야한다.");
-    res.send(null);
+    res.status(204).send(null);
   },
 };
