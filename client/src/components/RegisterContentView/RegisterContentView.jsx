8203facacf2f8fd2b9976ca859c1ed5743929817
sun-one95 <isay0693@gmail.com>
2022-04-08 13:33:50 +0900

    Update: 글 상세페이지 데이터 전달 및 zustand 에 상태 작성

--- client/src/components/RegisterContentView/RegisterContentView.jsx
+++ client/src/components/RegisterContentView/RegisterContentView.jsx
@@ -53,14 +53,15 @@ function DetailContent(props) {
         readOnly
       />
       <FlexBox>
-        <Img src="UserIcon.png" />
+        <Img src="UserIcon7.png" />
         <Div>{specificdata[0].jsonfile.nickName}</Div>
         <Div huge>2022-03-23</Div>
       </FlexBox>
       <FlexBox>
         <Div>사용 언어</Div>
-        <Language>React.js</Language>
-        <Language>Node.js</Language>
+        {specificdata[0].jsonfile.tag.map((el, idx) => {
+          return <Language key={idx}>{el}</Language>;
+        })}
         {specificdata[0].jsonfile.online ? (
           <Div
             backColor="rgba(196, 196, 196, 0.26)"
@@ -76,15 +77,13 @@ function DetailContent(props) {
         <Div backColor="rgba(196, 196, 196, 0.26)">
           {specificdata[0].jsonfile.date}
         </Div>
-      </FlexBox>
-      <ContentBlock>{specificdata[0].jsonfile.content}</ContentBlock>
-      <FlexBox top="2rem">
-        <Div>위치</Div>
+        <Div marginLeft="4rem">장소</Div>
         <Div backColor="rgba(196, 196, 196, 0.26)">
           {specificdata[0].jsonfile.roadAddress}{" "}
           {specificdata[0].jsonfile.placeName}
         </Div>
       </FlexBox>
+      <ContentBlock>{specificdata[0].jsonfile.content}</ContentBlock>
       <LikesViewShareIcon />
       <Comment />
     </Section>
