b51c46222baee3faf3e9d4ba9529b1a634c41259
happy5happy5 <zombil8731@gmail.com>
2022-04-10 15:43:33 +0900

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,7 +65,6 @@ function App() {
       // .then((x) =>
       //   x.status === 200 ? [chgInput(x.data), chgIsLogin(true)] : null
       // );
-      console.log(temp);
       if (temp.status === 200) {
         chgInput(temp.data);
         chgIsLogin(true);
